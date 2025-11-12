2f87737819f1fb6d75d4e5f5506dd22bffd2065e
patrickbrophy <bropy009@mymail.unisa.edu.au>
2025-11-12 16:50:23 +1100

    Developed function to assign an animal to an enclosure.

--- enclosure.py
+++ enclosure.py
@@ -13,26 +13,34 @@ class Enclosure:
         self.__id = f"Enclosure.{Enclosure.counter}"
         self.__biome = biome
         self.__is_clean = True
-        self.__has_animal = False
+        self.__occupancy = False
         self.__size = f"{size}m\u00b2"
         Enclosure.counter += 1
         enclosure_register[self.__id] = self
 
     def get_enclosure_id(self) -> str:
         return self.__id
-    def get_enclosure_name(self) -> str:
+    def get_enclosure_biome(self) -> str:
         return self.__biome
+    def get_occupancy(self) -> bool:
+        return self.__occupancy
+    def set_occupancy(self, animal_ID = None) -> None:
+        if animal_ID is not None:
+            self.__occupancy = animal_ID
+        else:
+            self.__occupancy = False
+
 
     @staticmethod
     def get_enclosure_data():
         enclosure_data = []
         for key, value in enclosure_register.items():
-            enclosure_data.append(f"{key:<13}: {value.__biome:<11} {'Clean 'if value.__is_clean else 'Dirty':<9} {value.__size:<8} {value.__has_animal if value.__has_animal else 'Empty'}")
+            occupancy = value.get_occupancy() if value.get_occupancy() else 'Empty'
+            enclosure_data.append(f"{key:<13}: {value.__biome:<11} {'Clean 'if value.__is_clean else 'Dirty':<9} {value.__size:<8} {occupancy}")
         return "\n".join(enclosure_data)
 
     def change_enclosure_cleanliness(self):
-        if self.__is_clean:
-            self.__is_clean = False
-        else:
-            self.__is_clean = True
+        self.__is_clean = not self.__is_clean
+
+
 
